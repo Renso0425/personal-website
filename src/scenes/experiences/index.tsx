6ddeb203f31cf49d7626cbab4d786ee98fc42191
Renso0425 <141680941+Renso0425@users.noreply.github.com>
2024-12-30 15:49:36 -0500

    Update index.tsx

--- src/scenes/experiences/index.tsx
+++ src/scenes/experiences/index.tsx
@@ -9,19 +9,19 @@ const experiences: Array<ExperienceType> = [
         title: "Historian",
         time: "May 2024 - Present",
         place: "College Park, MD",
-        description: "Developed and maintained a website using HTML and CSS to promote the organization’s activities. Took, edited, and curated photos and videos to advertise the organization’s events. Collaborated and planned with like-minded individuals in celebrating Chinese culture."
+        description: "Developed and maintained a website using HTML and CSS to promote the organization's activities. Captured, edited, and curated photos and videos to advertise the organization's events. Collaborated with like-minded individuals to plan and celebrate Chinese culture."
     },
     {
         title: "Community Assistant",
         time: "August 2022 - Present ",
         place: "College Park, MD",
-        description: "Collected, organized, and distributed packages and keys for student residents. Maintained a positive representation for the university. Provided any kind of assistance to students, residential life, parents, and package carriers. Played a reliable connection between 300 residents and the residential department of the school"
+        description: "Collected, organized, and distributed packages and keys for student residents. Maintained a positive representation of the university. Provided assistance to students, residential life staff, parents, and package carriers. Acted as a reliable liaison between 300 residents and the school's residential department."
     },
     {
         title: "Waiter",
         time: "June 2022 - August 2022",
         place: "Warner Robins, GA",
-        description: "Took and distributed customer orders in person and for delivery. Greeted and seated dine-in customers, making sure every customer is satisfied with their experience. Maintained organization among assigning tables for waiters."
+        description: "Received and processed customer orders for both in-person dining and delivery. Greeted and seated dine-in customers, ensuring they were satisfied with their experience. Maintained organization by efficiently assigning tables to waitstaff."
     },
 ];
 
@@ -65,7 +65,7 @@ const Experiences = ({ setSelectedPage }: Props) => {
             {/* Experiences */}
             {/*md means when it goes to a bigger screen*/}
             <motion.div 
-                className="mt-5 items-center justify-between gap-8 md:flex"
+                className="mt-5 items-center justify-between gap-8 md:grid md:grid-cols-3"
                 initial="hidden"
                 whileInView="visible"
                 viewport={{ once: false, amount: 0.5}}
